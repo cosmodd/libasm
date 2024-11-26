275190592ccabc413295e185a6d854902c922c03
Maxim <lolesqua@gmail.com>
2024-11-26 10:44:18 +0100

    return -1 when failed to read

--- src/ft_read.s
+++ src/ft_read.s
@@ -3,6 +3,12 @@ global ft_read
 ; ssize_t ft_read(int fd, void *buf, size_t count);
 section .text
 ft_read:
-	mov rax, 0 ; set rax to sys_read
+	mov rax, 0 ; sys_read
 	syscall
+	test rax, rax
+	js ft_read_error
+	ret
+
+ft_read_error:
+	mov rax, -1
 	ret
