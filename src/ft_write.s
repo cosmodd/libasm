3ae3cf85d09cf860765fe4b4785a6567deea591c
Maxim <lolesqua@gmail.com>
2024-11-26 11:13:53 +0100

    Handling error case for ft_write

--- src/ft_write.s
+++ src/ft_write.s
@@ -5,4 +5,10 @@ section .text
 ft_write:
 	mov rax, 1 ; set rax (system call) to 1 (sys_write)
 	syscall ; call write
+	test rax, rax
+	js ft_write_error
 	ret ; return syscall return value (set in rax)
+
+ft_write_error:
+	mov rax, -1
+	ret
